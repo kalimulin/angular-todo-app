98d639e95c48b243d69a10ad888a438185838251
kalimulin <kalimulin-ru@yandex.ru>
2020-04-14 21:41:34 +0400

    сортировка и пагинация списка задач

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Task} from "../../model/Task";
 import {DataHandlerService} from "../../service/data-handler.service";
 import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-tasks',
@@ -14,11 +16,15 @@ export class TasksComponent implements OnInit {
   displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
   dataSource: MatTableDataSource<Task>;
 
+  // ссылки на компоненты таблицы
+  @ViewChild(MatPaginator, {static: false}) private paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) private sort: MatSort;
+
   tasks: Task[];
 
   constructor(private dataHandler: DataHandlerService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.dataHandler.tasksSubject.subscribe(tasks => {
       this.tasks = tasks;
     });
@@ -29,7 +35,11 @@ export class TasksComponent implements OnInit {
     this.refreshTable();
   }
 
-  toggleTaskCompleted(task: Task) {
+  ngAfterViewInit() {
+    this.addTableObjects();
+  }
+
+  toggleTaskCompleted (task: Task) {
     task.completed = !task.completed;
   }
 
@@ -47,5 +57,28 @@ export class TasksComponent implements OnInit {
   // показ задач по всем текущим условиям
   private refreshTable() {
     this.dataSource.data = this.tasks;
+    this.addTableObjects();
+
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+      switch (colName) {
+        case 'priority': {
+          return task.priority ? task.priority.id : null;
+        }
+        case 'category': {
+          return task.category ? task.category.title : null;
+        }
+        case 'date': {
+          return task.date ? task.date : null;
+        }
+        case 'title': {
+          return task.title;
+        }
+      }
+    }
+  }
+
+  private addTableObjects() {
+    this.dataSource.sort = this.sort; // компонент для сортировки данных
+    this.dataSource.paginator = this.paginator; // обновление компонента постраничности
   }
 }
